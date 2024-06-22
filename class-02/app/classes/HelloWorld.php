1830c0591cda03a241423467b0f17550c6068e7a
MD SOHEL RANA <132728416+sohelrana626@users.noreply.github.com>
2024-06-23 00:32:12 +0600

    Basic class and files

---
+++ class-02/app/classes/HelloWorld.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\classes;
+
+
+class HelloWorld
+    {
+    public $message;
+
+    public function __construct()
+        {
+            $this->message='Hello World';
+        }
+
+    public function index()
+        {
+            echo $this->message;
+        }
+
+
+}
